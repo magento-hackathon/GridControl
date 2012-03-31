a2da120295bee78bdc2fb98f4c57ace0068e0fcd
Bastian Ike <thebod@thebod.de>
2012-03-31 13:20:09 +0200

    observer skeleton

---
+++ src/app/code/community/Hackathon/GridControl/Model/Observer.php
@@ -0,0 +1,15 @@
+<?php
+
+class Hackathon_GridControl_Model_Observer
+{
+    /**
+     * observe before_block_to_html
+     *
+     * @param Varien_Event_Observer $event
+     * @return void
+     */
+    public function beforeBlockToHtml(Varien_Event_Observer $event)
+    {
+
+    }
+}
