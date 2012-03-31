b347b303d112e1ed01df5a31f5db442991baada3
Bastian Ike <thebod@thebod.de>
2012-03-31 13:24:02 +0200

    changes beforeBlockToHtml to adminhtmlBlockHtmlBefore

--- src/app/code/community/Hackathon/GridControl/Model/Observer.php
+++ src/app/code/community/Hackathon/GridControl/Model/Observer.php
@@ -3,12 +3,12 @@
 class Hackathon_GridControl_Model_Observer
 {
     /**
-     * observe before_block_to_html
+     * observe adminhtml_block_html_before
      *
      * @param Varien_Event_Observer $event
      * @return void
      */
-    public function beforeBlockToHtml(Varien_Event_Observer $event)
+    public function adminhtmlBlockHtmlBefore(Varien_Event_Observer $event)
     {
 
     }
